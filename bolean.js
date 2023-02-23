8dc09c84772a8ba02035bbc6f5e7dd1734662585
Anthony-Idoko <tidoko10@gmail.com>
2023-02-23 09:37:35 +0000

    updated

--- bolean.js
+++ bolean.js
@@ -1,20 +1,20 @@
 //Bolean datatype
-// var bolean = 'true';
-// console.log(bolean);
+var bolean = 'true';
+console.log(bolean);
 
-// if (bolean = false){
-// 	console.log('This is true.');
-// }else{
-// 	console.log('This is not true.');
-// }
-
-var a = 11;
-var	b = 1;
-
-if (a == b){
-	console.log('This is correct');
-	console.log(a + b);
+if (bolean = false){
+	console.log('This is true.');
 }else{
-	console.log('it is not correct');
+	console.log('This is not true.');
 }
+
+// var a = 11;
+// var	b = 1;
+
+// if (a == b){
+// 	console.log('This is correct');
+// 	console.log(a + b);
+// }else{
+// 	console.log('it is not correct');
+// }
 
