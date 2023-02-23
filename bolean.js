890d8cf454fc0f6eaf1f3486aaf404122886c652
Anthony-Idoko <tidoko10@gmail.com>
2023-02-23 09:40:53 +0000

    updated

--- bolean.js
+++ bolean.js
@@ -1,4 +1,5 @@
 //Bolean datatype
+
 var bolean = 'true';
 console.log(bolean);
 
@@ -8,13 +9,5 @@ if (bolean = false){
 	console.log('This is not true.');
 }
 
-// var a = 11;
-// var	b = 1;
 
-// if (a == b){
-// 	console.log('This is correct');
-// 	console.log(a + b);
-// }else{
-// 	console.log('it is not correct');
-// }
 
